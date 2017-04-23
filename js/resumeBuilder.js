423492738fe7bafe997429b47180e29c0a4a0c98
Hazelton Jacob <jhazelton11@gmail.com>
2017-04-23 19:10:16 +0900

    Javascript tutorial at the start of Lesson 5

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,82 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work {
+  "jobs" : [
+    {
+      "name" : "YBM ECC",
+      "title" : "Native Teacher",
+      "location" : "Siheung-Si, Gyeonggi-Do, South Korea",
+      "dates" : "October 2014 - October 2015",
+      "description" : "Taught English to children aged 8 - 16"
+    },
+    {
+    "name" : "Rise Bundang",
+    "title" : "Native Teacher",
+    "location" : "Seongnam-Si, Gyeonggi-Do, South Korea",
+    "dates" : "January 2016 - Present",
+    "description" : "Taught English to children aged 6 - 10"
+    }
+  ]
+}
+
+var projects {
+  "projects" : [
+    {
+      "title" : "Learn Enough HTML to be Dangerous",
+      "dates" : "March 2017",
+      "description" : "Michael Hartl's Learn Enough HTML to be Dangerous tutorial",
+      "images" : ["https://jhazelton1.github.io/sample_website/"]
+    },
+    {
+      "title" : "Learn Enough CSS to be Dangerous",
+      "dates" : "April 2017 - Present",
+      "description" : "Michael Hartl's Learn Enough CSS to be Dangerous tutorial",
+      "images" : ["https://jhazelton1.github.io/sample_css/"]
+    }
+  ]
+}
+
+var bio {
+  "name" : "Jacob Hazelton",
+  "role" : "Aspiring Programmer",
+  "welcomeMessage" : "Welcome to my resume's website",
+  "biopic" : "String value",
+  "contacts" : {
+    "mobile" : "555-5555-5555",
+    "email"  : "example@gmail.com",
+    "github" : "jhazelton1",
+    "twitter" : "@JacobHazelton",
+    "location" : "Seoul, South Korea"
+  },
+  "skills" : ["JavaScript", "CSS", "HTML", "Java"]
+}
+
+var education {
+  "schools" : [
+    {
+    "name" : "Georgia State Universtiy",
+    "location" : "Atlanta, GA, US",
+    "degree dates" : "2008 - 2012",
+    "url" : "www.gsu.edu",
+    "majors" : ["Computer Information Systems"]
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "title" : "Learn Enough HTML to be Dangerous",
+      "school" : "Learn Enough Society",
+      "dates" : "March 2017",
+      "url" : "https://www.learnenough.com/html-tutorial"
+    },
+    {
+      "title" : "Learn Enough Git to be Dangerous",
+      "school" : "Learn Enough Society",
+      "dates" : "February 2017",
+      "url" : "https://www.learnenough.com/git-tutorial"
+    },
+    {
+      "title" : "Learn Enough Command Line to be Dangerous",
+      "school" : "Learn Enough Society",
+      "dates" : "January 2017",
+      "url" : "https://www.learnenough.com/command-line-tutorial"
+    }
+  ]
+}
